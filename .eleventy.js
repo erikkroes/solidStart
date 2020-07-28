68fc21141ac17cef2d16632ffc8cb8918c7bda53
Erik Kroes <github@k4rp.nl>
2020-07-28 13:54:45 +0200

    feat: favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,10 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("site.webmanifest");
 };
